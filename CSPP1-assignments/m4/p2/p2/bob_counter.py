95bf9aa87077b7dec03dee4a505269fa6381af91
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 09:27:00 +0530

    " -> 5 of 5 passed. pylint: 5.56/10 "

--- CSPP1-assignments/m4/p2/p2/bob_counter.py
+++ CSPP1-assignments/m4/p2/p2/bob_counter.py
@@ -1,6 +1,6 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+program to print the number of times the string 'bob' occurs in s
 
 Number of times bob occurs is: 2'''
 
