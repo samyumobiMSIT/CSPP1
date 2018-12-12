92db069006a7cd770a6b05348bc454c4c9068923
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:36:45 +0530

    " -> 0 of 3 passed. pylint: 0.00/10 "

--- CSPP1-assignments/module 5/ag4/square_root_newtonrapson.py
+++ CSPP1-assignments/module 5/ag4/square_root_newtonrapson.py
@@ -11,14 +11,14 @@ using approximation method '''
 
 def main():
 	'''main function'''
-	s = input()
+	k = input()
 	# epsilon and step are initialized
 	# don't change these values
 	epsilon = 0.01
 	# your code starts here
 	# Find x such that x**2 - 24 is within epsilon of 0
 
-	k = 24.0
+
 	guess = k / 2.0
 	while abs(guess * guess - k) >= epsilon:
 		guess = guess - (((guess ** 2) - k) / (2 * guess))
