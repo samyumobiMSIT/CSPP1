f9c8dcd55feb6a3d39cc2984f85fd3d50dc0abec
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:46:59 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

---
+++ CSPP1-assignments/m6/p2/special_char.py
@@ -0,0 +1,13 @@
+'''
+Replace all the special characters(!, @, #, $, %, ^, &, *) in a given string with a space.
+example : ab!@#cd is the input, the output is ab   cd
+Output has three spaces, which are to be replaced with these special characters
+'''
+def main():
+    '''
+    Read string from the input, store it in variable str_input.
+    '''
+    str_input = input()
+
+if __name__ == "__main__":
+    main()
---
+++ CSPP1-assignments/m6/p3/digit_product.py
@@ -0,0 +1,15 @@
+'''
+Given a  number int_input, find the product of all the digits
+example: 
+	input: 123
+	output: 6
+'''
+def main():
+    '''
+    Read any number from the input, store it in variable int_input.
+    '''
+    int_input = int(input())
+
+
+if __name__ == "__main__":
+    main()
