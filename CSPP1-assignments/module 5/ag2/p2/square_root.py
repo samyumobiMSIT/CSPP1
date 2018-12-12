e337b3b17f3dbba1683dd81f5f0d80bfd2bab4d0
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:27:18 +0530

    " -> 3 of 3 passed. pylint: 9.29/10 "

--- CSPP1-assignments/module 5/ag2/p2/square_root.py
+++ CSPP1-assignments/module 5/ag2/p2/square_root.py
@@ -10,25 +10,22 @@
 # output: 6.999999999999991
 
 def main():
-	s = input()
-	# epsilon and step are initialized
-	# don't change these values
-	x=25
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-	numGuesses = 0
-	ans = 0.0
-	while abs(ans ** 2 - x) >= epsilon and ans <= x:
-		ans += step
-		numGuesses += 1
-	print('numGuesses =', numGuesses)
+    '''This is main function'''
+    square_num = int(input())
+    aprroximation_val = 0.01
+    step_val = 0.1
+    guess_num = 0.0
 
-	if abs(ans ** 2 - x) >= epsilon:
-		print('Failed on square root of', x)
+    while guess_num <= square_num:
+        if abs(guess_num**2 -square_num) < aprroximation_val:
+            break
+        else:
+            guess_num += step_val
 
-	else:
-		print(ans, 'is close to square root of', x)
+    if abs(guess_num**2 - square_num) >= aprroximation_val:
+        print('fail')
+    else:
+        print(str(guess_num))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
