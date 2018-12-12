3019366dab262068f5d9b5af3f8674fcffbc41ba
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 09:33:28 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- CSPP1-assignments/m4/p2/p2/bob_counter.py
+++ CSPP1-assignments/m4/p2/p2/bob_counter.py
@@ -6,10 +6,10 @@ Number of times bob occurs is: 2'''
 def main():
     '''This is main function'''
     s_s = input()
-    count_bob= 0
+    count_bob = 0
     for i in range(len(s_s)):
         if s_s[i:].startswith('bob'):
-            count_bob+= 1
+            count_bob += 1
     print(str(count_bob))
 
 if __name__ == "__main__":
