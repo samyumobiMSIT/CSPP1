24ca9cac2572fa340263bf20358c8ef2624099d1
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 09:29:07 +0530

    " -> 5 of 5 passed. pylint: 6.67/10 "

--- CSPP1-assignments/m4/p2/p2/bob_counter.py
+++ CSPP1-assignments/m4/p2/p2/bob_counter.py
@@ -5,12 +5,12 @@ program to print the number of times the string 'bob' occurs in s
 Number of times bob occurs is: 2'''
 
 def main():
-    s = input()
-    countBob = 0
-    for i in range(len(s)):
-        if s[i:].startswith('bob'):
-            countBob += 1
-    print(str(countBob))
+    s_s = input()
+    count_Bob = 0
+    for i in range(len(s_s)):
+        if s_s[i:].startswith('bob'):
+            count_Bob += 1
+    print(str(count_Bob))
 
 if __name__ == "__main__":
     main()
