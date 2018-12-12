8a98da698745084736e87acb9f89f855d5afa5d0
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 09:35:56 +0530

    " -> 5 of 5 passed. pylint: 9.33/10 "

--- CSPP1-assignments/m4/p3/p3/longest_substring.py
+++ CSPP1-assignments/m4/p3/p3/longest_substring.py
@@ -1,36 +1,26 @@
 '''Assume s is a string of lower case characters.
-
-Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
-For example, if s = 'azcbobobegghakl', then your program should print
-
-Longest substring in alphabetical order is: beggh
-
-In the case of ties, print the first substring.
-For example, if s = 'abcbcd', then your program should print
-
-Longest substring in alphabetical order is: abc
-
-Note: This problem may be challenging. We encourage you to work smart.
-If you've spent more than a few hours on this problem, we suggest that you move on to a different part of the course.
-If you have time, come back to this problem after you've had a break and cleared your head.'''
-
-from itertools import count
+program that prints the longest substring of s in which the letters occur in alphabetical order.
+'''
+#longest string in alphabetical order
 
 def main():
-	s = input()
-	# the input string is in s
-	# remove pass and start your code here
-	maxSubstr = s[0:0]
-	for start in range(len(s)):
-		for end in count(start + len(maxSubstr) + 1):
-			substr = s[start:end]
-			if len(substr) != (end - start):
-				break
-			if sorted(substr) == list(substr):
-				maxSubstr = substr
+    '''this is main function'''
+    in_strs = input()
+    # the input string is in s
+    # remove pass and start your code here
+    count_1 = 0
+    count_max = 0
+    end_index = 0
+    for index_strs in range(len(in_strs)-1):
+        if ord(in_strs[index_strs]) <= ord(in_strs[index_strs+1]):
+            count_1 += 1
+        else:
+            count_1 = 0
+        if count_max < count_1:
+            count_max = count_1
+            end_index = index_strs + 1
+    print(in_strs[end_index - count_max : end_index + 1])
 
-	print("Longest substring in alphabetical order is: " + str(maxSubstr))
+if __name__ == "__main__":
+    main()
 
-
-if __name__== "__main__":
-	main()
