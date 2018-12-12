a84112192d31ea51fb5ff809fe813feff65c69dd
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:59:33 +0530

    " -> 10 of 10 passed. pylint: 9.41/10 "

--- CSPP1-assignments/module 7/Functions - Assignment-1/assignment1.py
+++ CSPP1-assignments/module 7/Functions - Assignment-1/assignment1.py
@@ -2,26 +2,6 @@
 
 program to calculate the credit card balance after one year'''
 
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-# monthlyPaymentRate - minimum monthly payment rate as a decimal
-
-# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
-# balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
-# Remaining balance: 813.41
-# instead of
-# Remaining balance: 813.4141998135
-
-# So your program only prints out one thing: the remaining balance at the end of the year in the format:
-# Remaining balance: 4784.0
-
-# A summary of the required math is found below:
-# Monthly interest rate= (Annual interest rate) / 12.0
-# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
 
 
 def paying_debt_off_inayear(balance_unpaid, annual_interest_rate, monthly_payment_rate):
