54f971acf9aeab426cd9e6d550843cac0f6821aa
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:50:54 +0530

    " -> 7 of 7 passed. pylint: 9.23/10 "

--- CSPP1-assignments/m6/p3/digit_product.py
+++ CSPP1-assignments/m6/p3/digit_product.py
@@ -6,10 +6,23 @@ example:
 '''
 def main():
     '''
-    Read any number from the input, store it in variable int_input.
-    '''
-    int_input = int(input())
+        Read any number from the input, store it in variable int_input.
+        '''
+    int_input = input()
 
+    digits_product = 1
+    flag = 0
+
+    for input_iterator in int_input:
+        if input_iterator == '-':
+            flag = 1
+        else:
+            digits_product *= int(input_iterator)
+
+    if flag == 1:
+        print("-" + str(digits_product))
+    else:
+        print(digits_product)
 
 if __name__ == "__main__":
     main()
