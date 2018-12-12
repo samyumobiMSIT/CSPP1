014c8aa91d0af9598b8ce781ea84aae7cb6f34bc
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-12 11:48:42 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- CSPP1-assignments/m6/p2/special_char.py
+++ CSPP1-assignments/m6/p2/special_char.py
@@ -5,9 +5,16 @@ Output has three spaces, which are to be replaced with these special characters
 '''
 def main():
     '''
-    Read string from the input, store it in variable str_input.
-    '''
+        Read string from the input, store it in variable str_input.
+        '''
     str_input = input()
+    output_str = ""
+    for str_iterator in str_input:
+        if str_iterator in "!@#$%^&*":
+            output_str += " "
+        else:
+            output_str += str_iterator
+    print(output_str)
 
 if __name__ == "__main__":
     main()
