f083dcc0e8b8ac534b3d8c070d63f38595f80512
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-19 12:25:56 +0530

    cspp

---
+++ CSPP1-practice/m3/compare_AB.py
@@ -0,0 +1,11 @@
+VARA = "abc"
+VARB = "abcd"
+
+if isinstance(VARA, str) or isinstance(VARB, str) is str:
+    print("string involved")
+elif VARA > VARB:
+    print("bigger")
+elif VARA == VARB:
+    print("equal")
+else:
+    print("smaller")
---
+++ CSPP1-practice/m3/iterate_even_reverse.py
@@ -0,0 +1,6 @@
+#program to print even numbers in reverse order
+
+print("Hello!")
+
+for I in range(10, 1, -2):
+    print(I)
