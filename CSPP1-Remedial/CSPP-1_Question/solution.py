d82e28fb7e22d923253927c3e210a56739766330
samyumobiMSIT <samyumobi@msitprogram.net>
2018-12-21 10:36:44 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -5/10 "

--- CSPP1-Remedial/CSPP-1_Question/solution.py
+++ CSPP1-Remedial/CSPP-1_Question/solution.py
@@ -1,117 +1,26 @@
-"""
-Program to solve sudoku from input file.
-"""
 import sys
-import time
-import numpy as np
 
+def same_row(i,j): return (i/9 == j/9)
+def same_col(i,j): return (i-j) % 9 == 0
+def same_block(i,j): return (i/27 == j/27 and i%9/3 == j%9/3)
 
-def read_input(filename):
-    """Take file name and return 2d array = board"""
-    with open(str(filename), 'r') as file:
-        board = np.array([line.split() for line in file]).astype(int)
-    return board
+def r(a):
+  i = a.find('0')
+  if i == -1:
+    sys.exit(a)
 
+  excluded_numbers = set()
+  for j in range(81):
+    if same_row(i,j) or same_col(i,j) or same_block(i,j):
+      excluded_numbers.add(a[j])
 
-def print_board(board):
-    """Take 2d array board and print"""
-    for i in range(len(board)):
-        for j in range(len(board[:])):
-            print(str(board[i, j]) + " ", end='')
-            # print(str(type(board[i,j])) + " ",end='' )
-        print()
+  for m in '123456789':
+    if m not in excluded_numbers:
+      r(a[:i]+m+a[i+1:])
 
-
-def naive_solver(board):
-    """Naive approach to solving sudoku board"""
-    cell_counter = 1  # cell counter
-    init_board = np.copy(board)  # make copy of initial board
-    forward = 1  # keep track of direction moving on board
-
-    # main solving loop
-    while cell_counter > 0 and cell_counter < 82:
-        # case 1: original cell was empty and 9 hasn't been tested
-        # print(int((cell_counter-1)/9), (cell_counter-1)%9)
-        if (init_board[int((cell_counter - 1) / 9), (cell_counter - 1) % 9] == 0 and
-                board[int((cell_counter - 1) / 9), (cell_counter - 1) % 9] < 9):
-            # print('case1 ' + str(cell_counter))
-            forward = 1
-            board[int((cell_counter - 1) / 9), (cell_counter - 1) % 9] += 1
-            if not any_duplicates(board):
-                cell_counter += 1
-        # case 2: original cell was empty, but have tried all numbers
-        elif (init_board[int((cell_counter - 1) / 9), (cell_counter - 1) % 9] == 0):
-            board[int((cell_counter - 1) / 9), (cell_counter - 1) % 9] = 0
-            # print('case2 ' + str(cell_counter))
-            cell_counter -= 1
-            forward = -1
-        # case 3: cell was filled to start
-        else:
-            # print('case3 ' + str(cell_counter))
-            if forward == 1:
-                cell_counter += 1
-            else:
-                cell_counter -= 1
-
-    # Check if solution or not
-    if cell_counter == 0:
-        raise ValueError("Board has no solution")
-
-
-def any_duplicates(board):
-    """Check all rows, columns, and 9x9 squares for duplicates."""
-    # Check all value
-    for num in range(1, 10):
-        # Check rows and column
-        for i in range(0, 9):
-            row_val_count = len(list(map(str, np.where(board[:, i] == num)[0])))
-            col_val_count = len(list(map(str, np.where(board[i, :] == num)[0])))
-            if row_val_count > 1 or col_val_count > 1:
-                return True
-        # Check boxes of 9
-        box_val_count = []
-        box_val_count.append(len(list(map(str, np.where(board[0:3, 0:3] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[0:3, 3:6] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[0:3, 6:9] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[3:6, 0:3] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[3:6, 3:6] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[3:6, 6:9] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[6:9, 0:3] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[6:9, 3:6] == num)[0]))))
-        box_val_count.append(len(list(map(str, np.where(board[6:9, 6:9] == num)[0]))))
-
-        for val in box_val_count:
-            if val > 1:
-                return True
-
-    # If no duplicates, return false
-    return False
-
-
-def main():
-    """Main program to solve sudoku from input file."""
-    # Read file as input argument
-    # TODO: Generalize input
-    time_start = time.time()
-
-    filename = sys.argv[1]
-    print(str(filename))
-    board = read_input(str(filename))
-
-    # Print inital board
-    print("Starting board:")
-    print_board(board)
-    print()
-
-    # run solver
-    naive_solver(board)
-    time_end = time.time()
-
-    # Print final board (solved or unsolved)
-    print("Final board:")
-    print_board(board)
-    print("Solution took %.2f seconds" % (time_end - time_start))
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+  if len(sys.argv) == 2 and len(sys.argv[1]) == 81:
+    r(sys.argv[1])
+  else:
+    print('Usage: python sudoku.py puzzle')
+   
