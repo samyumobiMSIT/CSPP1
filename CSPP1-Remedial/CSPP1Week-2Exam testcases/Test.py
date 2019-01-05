f45f732f60454d1d8448e77f58f1e9d44c500079
samyumobiMSIT <samyumobi@msitprogram.net>
2019-01-05 12:31:47 +0530

    "cspp1_m10_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 1/10 "

--- CSPP1-Remedial/CSPP1Week-2Exam testcases/Test.py
+++ CSPP1-Remedial/CSPP1Week-2Exam testcases/Test.py
@@ -1,35 +1,36 @@
+'''count levels'''
+def levels(l):
+    ls = " "
+    c = 0
+    #additional list to capture [
+    ls = ''.join(str(p) for p in l)
+    for j in range(len(ls)):
+        if ls[j] == '[':
+            c += 1
+    return c
+
+'''count sum'''
 def sum_list(l):
     s=0
     for i in l:
+        #check if i is there in list and calc sum
         if isinstance(i,list):
           s += sum_list(i)
         else:
             try:
                 s+=i
+                '''except typeerrors'''
             except TypeError:
               continue
     return s
 
-def levels(l):
-    s = " "
-    c=0
-    s = ''.join(str(p) for p in l)
-    for j in range(len(s)):
-        if s[j] == '[':
-            c+=1
-    return c
-
-
-
-
-
-
+'''main'''
 def main():
     lst = eval(input())
     print(sum_list(lst))
     print(levels(lst))
 
-
+'''cmd'''
 if __name__ == '__main__':
     main()
 
